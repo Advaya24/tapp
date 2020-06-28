1ae0e51fcb6f67ab8d42762204aa64fdb0a31b63
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-28 15:00:36 +0000

    Deploy uoft-tapp/tapp to github.com/uoft-tapp/tapp.git:gh-pages

---
+++ precache-manifest.5a75d3971912aef99a3cfcd7cfaac6d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5236128a006554c87c94b30e735b63f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "402af11681b7ea31d2de",
+    "url": "./static/css/2.a06662c6.chunk.css"
+  },
+  {
+    "revision": "30692143acf95d1351c8",
+    "url": "./static/css/main.a8a89729.chunk.css"
+  },
+  {
+    "revision": "402af11681b7ea31d2de",
+    "url": "./static/js/2.d99646c1.chunk.js"
+  },
+  {
+    "revision": "f202e3206c8d5b2e509bea3f78932238",
+    "url": "./static/js/2.d99646c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30692143acf95d1351c8",
+    "url": "./static/js/main.10e588c3.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  }
+]);
